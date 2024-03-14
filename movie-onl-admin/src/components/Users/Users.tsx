1e51f66f0f5cf3cb7c205c26a56f0f7eaea74097
hieu <hieuvv29899@gmail.com>
2024-03-14 16:17:22 +0700

    updateadminpage

---
+++ movie-onl-admin/src/components/Users/Users.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <div>
+      <section className="content">
+        <h2>Users Management</h2>
+        <hr style={{ margin: "20px 0" }} />
+
+      </section>
+    </div>
+  );
+};
+
+export default Users;
